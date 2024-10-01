1dc924e7520a2172618b450b7a5b8b1e694347f0
anas <anasoudsassi@gmail.com>
2024-10-01 01:17:23 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      },
+      colors:{
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F'
+      },
+      boxShadow:{
+        'black': '4px 4px 0 #000',
+        'white': '4px 4px 0 #fff',
+      }
+    },
+    
+  },
+  darkMode: 'selector',
+  
+  plugins: [],
+}
+
